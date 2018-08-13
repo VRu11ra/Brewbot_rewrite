d6df37e57d766c60f655a11c60b685fb4b8c3502
Artur Janik <vru11ra@gmail.com>
2018-08-13 03:04:53 +0000

    added lobbies

--- main.js
+++ main.js
@@ -2,12 +2,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { spawn, execFile } = require('child_process');
+const _ = require('underscore');
 var sz = true;
 var aggrsz = true;
 var timeout = 5000;
 var aggrtimeout = 10000;
 var self = 'BrewBot';
-
+if (process.argv[3] == 'f'){
+	sz = false;
+	console.log('sanitization turned off');
+}
+if (process.argv[4] == 'f'){
+	aggrsz = false;
+	console.log('aggressive sanitization turned off');
+}
 console.log(process.argv[3]); //TODO debug
 
 
@@ -21,15 +29,13 @@ client.on('message', msg => {
     msg.channel.send('pang');
   }
 
-
-
   //BOT SANITIZATION
   if(msg.author.bot === true){
 	if(aggrsz === true && !(msg.author.username === self)){
 		sanitize(msg, aggrtimeout);
 		msg.channel.send("```Aggressive sanitization is enabled. \nI will delete the above bot message in "+aggrtimeout/1000+" seconds.\nThis can be disabled by the dev in settings if this is a problem.```");
 	}
-	if(msg.author.username === self && !(msg.content.startsWith("**UNIQUE**"))){  //TODO laying framework for reaction based stuff
+	if(msg.author.username === self){  //TODO laying framework for reaction based stuff
 		sanitize(msg, timeout);
 	}
   }
@@ -63,6 +69,9 @@ client.on('message', msg => {
 			case 'mention':
 			case 'mn':
 				return "echo the following users have been mentioned in this message: "+ msg.mentions.users.array();
+			case 'lobby':
+			case 'lb':
+				return "echo **UNFULFILLED REQUEST** \n follwing users are being waited on: "+msg.mentions.users.array()+" by "+ msg.author+"```TO DISMISS THIS MESSAGE MENTIONED USERS CLICK THE EMOJI SIGNALLING THEIR PRESENCE\nTHIS MESSAGE WILL SELF DESTRUCT IN A HOUR```";
 			case 'rr':
 				return "echo this command is currently under development, check back later or pester the dev";
 			case 'sz':
@@ -71,8 +80,9 @@ client.on('message', msg => {
 				return "echo This command is not defined, check your spelling";
 		}
 	}
-//	console.log(response(procUsrIn));
+
 	var format = response(procUsrIn, msg).split(" ");
+	console.log(response);
 	var ls = spawn(format[0], format.slice(1));
 	ls.stdout.on('data', data => {
 		console.log(`stdout: ${data}`);
@@ -115,8 +125,28 @@ client.on('message', msg => {
 
 
 function sanitize(message, time){
-	if (sz === true){
-		message.delete(time);
+	if (sz === true && !(message.content.startsWith('**UNFULFILLED REQUEST**'))){
+		message.delete(time).catch(console.log("message was already deleted lol"));
 	}
+	if(message.content.startsWith('**UNFULFILLED REQUEST**')){
+		message.delete(360000);
+		message.react("1⃣").catch(function(){
+			console.log("got an issue with the emoji");
+		});
+//		const filter = (reaction, user, approved, creator) => reaction.emoji.name == "1⃣" && (user.id == approved || user.id == creator);
+//		message.awaitReactions(filter(message.mentions.users.array(), message.author), {time:1200000}).then(message.delete(2000)).catch(console.error); 
+
+		var reactors = [];
+		message.awaitReactions((reaction, user) => {
+			reactors.push(user);
+			console.log("reactors: "+ reactors);
+			console.log("mentioneds: "+ message.mentions.users.array());
+			if(_.difference(message.mentions.users.array(), reactors).length === 0 && reaction){
+				message.delete(timeout);
+				message.channel.send("**REQUEST FULFILLED**\n"+message.mentions.users.array() + "\n```Requested users are active\nRequest has been fulfilled. Have a nice day!```");
+			}
+		});
+	}
+
 }
 client.login(process.argv[2]);
